5a9779a05a117a5400d5a0b22651d40a1dfdd300
Mike Jolley <mike.jolley@me.com>
2017-03-22 13:19:42 +0000

    Use $is_dst in timezone_name_from_abbr

--- includes/wc-formatting-functions.php
+++ includes/wc-formatting-functions.php
@@ -557,13 +557,14 @@ function wc_timezone_string() {
 	// adjust UTC offset from hours to seconds
 	$utc_offset *= 3600;
 
+	// Detect DST.
+	$is_dst = (bool) date( 'I' );
+
 	// attempt to guess the timezone string from the UTC offset
-	$timezone = timezone_name_from_abbr( '', $utc_offset );
+	$timezone = timezone_name_from_abbr( '', $utc_offset, $is_dst );
 
 	// last try, guess timezone string manually
 	if ( false === $timezone ) {
-		$is_dst = (bool) date( 'I' );
-
 		foreach ( timezone_abbreviations_list() as $abbr ) {
 			foreach ( $abbr as $city ) {
 				if ( (bool) $city['dst'] === $is_dst && $city['offset'] == $utc_offset ) {
